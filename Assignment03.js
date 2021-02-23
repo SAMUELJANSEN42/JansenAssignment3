758718885ff0bf75d89d82a36a0d4fccd118354a
SAMUELJANSEN42 <sam.jansen@drake.edu>
2021-02-22 19:13:17 -0600

    update

--- Assignment03.js
+++ Assignment03.js
@@ -30,10 +30,10 @@ window.onload = function init()
 
     //note that this is no longer a local variable (removed the "var")
     vertices = [
-        vec2(0, 1),
-        vec2(-1, 0),
-        vec2(1, 0),
-        vec2(0, -1)
+        vec2(0, 0.25),
+        vec2(-0.25, 0),
+        vec2(0.25, 0),
+        vec2(0, -0.25)
     ];
     
     verticesTriangle = [
@@ -56,43 +56,32 @@ window.onload = function init()
 
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT);
-
-    // DRAW THE SQUARE
-    
+  
     gl.useProgram(program);
    
-    // Load the data 
     var bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
-    // Associate shader variables with our data bufferData
     var positionLoc = gl.getAttribLocation(program, "aPosition");
     gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc);
 
-    // rotation
-    theta += 0.01;
+    theta += 0.0;
     gl.uniform1f(thetaLoc, theta);
 
-    // DRAW IT!
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
-    // DRAW THE TRIANGLE
-    // switch to the Triangle shaders
     gl.useProgram(programTriangle);
     
-    // Load the data
     var bufferId2 = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId2);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(verticesTriangle), gl.STATIC_DRAW);
 
-    // Associate shader variables with our data bufferData
     var positionLoc2 = gl.getAttribLocation(programTriangle, "aPosition");
     gl.vertexAttribPointer(positionLoc2, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc2);
 
-    // DRAW IT!
     gl.drawArrays(gl.TRIANGLES, 0, verticesTriangle.length);
 
     requestAnimationFrame(render);
